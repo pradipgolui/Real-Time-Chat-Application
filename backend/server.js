5509b48a7c0afb1f43d41ba7a5940864036674f8
pradipgolui <your.email@example.com>
2025-04-08 03:16:19 +0530

    Basic server created

---
+++ backend/server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+const cors = require('cors');
+const PORT = process.env.PORT || 3000;
+
+const app = express();  
+const server = http.createServer(app);
+const io = socketIO(server, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST']
+    }
+}); 
+
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/',(req, res)=>{
+    res.send('API working......');
+})
+
+io.on('connection', (socket)=>{
+    console.log('New user connected');
+
+    socket.on('sendMessage',(data)=>{
+        io.emit('receiveMessage', data)
+    });
+
+    socket.on('disconnect',()=>{
+        console.log('User disconnected');
+    });
+});
+
+
+server.listen(PORT, ()=>{
+    console.log(`Server running at port ${PORT}`);
+});
