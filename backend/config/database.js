c9256b45c91b29879daacd13d05154deb457477a
pradipgolui <your.email@example.com>
2025-04-11 03:28:45 +0530

    user & message model added

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,17 +4,12 @@ require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-
+         await mongoose.connect(process.env.MONGO_URI, {
     });
-
-    console.log("MongoDB connected successfully");
-
+         console.log("MongoDB connected successfully");
   } catch (err) {
-
-    console.error("MongoDB connection failed:", err.message);
-    process.exit(1); 
-    
+         console.error("MongoDB connection failed:", err.message);
+         process.exit(1); 
   }
 };
 
